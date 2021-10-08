34a03403043d3cf51276c776ada311d149cd39fa
Reonxell <66777647+Reonxell@users.noreply.github.com>
2021-10-08 17:05:01 +0300

    Убрал лишнее

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,5 +1,3 @@
-from selenium.webdriver.common.by import By
-
 from .locators import MainPageLocators
 from .base_page import BasePage
 
@@ -11,3 +9,4 @@ class MainPage(BasePage):
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+
